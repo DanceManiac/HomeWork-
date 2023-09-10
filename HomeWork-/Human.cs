772699aebd486f13813a8b4cda3bb0590953988f
Dance Maniac <50047331+DanceManiac@users.noreply.github.com>
2023-09-10 11:12:18 +0300

    HomeWork4

---
+++ HomeWork-/Human.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Human
+{
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+    public Human Parent1 { get; set; }
+    public Human Parent2 { get; set; }
+
+    private static List<Human> humans = new List<Human>();
+
+    public Human(string name, string gender, int age)
+    {
+        Name = name;
+        Gender = gender;
+        Age = age;
+        humans.Add(this);
+    }
+
+    public static Human CreateHuman(string name, string gender, int age, Human parent1 = null, Human parent2 = null)
+    {
+        Human human = new Human(name, gender, age);
+        human.Parent1 = parent1;
+        human.Parent2 = parent2;
+        return human;
+    }
+
+    public static int GetCount()
+    {
+        return humans.Count;
+    }
+
+    public static string GetAllInfo()
+    {
+        string info = "";
+        foreach (Human human in humans)
+        {
+            info += $"Name: {human.Name}, Gender: {human.Gender}, Age: {human.Age}\n";
+            if (human.Parent1 != null)
+                info += $"Parent 1: {human.Parent1.Name}\n";
+            if (human.Parent2 != null)
+                info += $"Parent 2: {human.Parent2.Name}\n";
+            info += "\n";
+        }
+        return info;
+    }
+}
--- HomeWork-/Program.cs
+++ HomeWork-/Program.cs
@@ -10,6 +10,17 @@ namespace HomeWork_
     {
         static void Main(string[] args)
         {
+            Human parent1 = new Human("Father", "male", 50);
+            Human parent2 = new Human("Mother", "female", 45);
+            Human.CreateHuman("Child1", "male", 20, parent1, parent2);
+            Human.CreateHuman("Child2", "female", 18, parent1, parent2);
+            Human.CreateHuman("Child3", "male", 15, parent1, parent2);
+            Human.CreateHuman("Child4", "female", 12, parent1, parent2);
+            Human.CreateHuman("Child5", "male", 5);
+
+            Console.WriteLine($"Total number of humans: {Human.GetCount()}\n");
+
+            Console.WriteLine(Human.GetAllInfo());
         }
     }
 }
